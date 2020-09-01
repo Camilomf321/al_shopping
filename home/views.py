fc03fb056628ae2eda78d83415d761660f0ade9d
alda27 <amunozro8970@universidadean.edu.co>
2020-09-01 12:14:33 -0500

    added home app

---
+++ home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from shop.models import Category, Product
+
+
+# Create your views here.
+def home(request):
+    latest_products = Product.product_available.all()[:6]
+    products_features = Product.product_featured.all()[:8]
+    context = {'latest_products': latest_products,
+               'products_features': products_features
+               }
+    return render(request, 'home/home.html', context)
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
